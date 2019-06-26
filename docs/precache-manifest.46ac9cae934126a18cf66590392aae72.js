f888afbecddeb37f319df66823628ce773cb62a3
toutpt <toutpt@gmail.com>
2019-06-26 11:00:39 +0200

    docs: update

---
+++ docs/precache-manifest.46ac9cae934126a18cf66590392aae72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "471ea4443910f596e35e7960d2a17805",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1ec31d48acf51f8171a",
+    "url": "/static/css/main.4d1340e1.chunk.css"
+  },
+  {
+    "revision": "a95b1e422ace2989e56e",
+    "url": "/static/js/2.d0fe087c.chunk.js"
+  },
+  {
+    "revision": "f1ec31d48acf51f8171a",
+    "url": "/static/js/main.4b2945d5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
